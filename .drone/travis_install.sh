86d6f1626bd8c96fb76ace1b9bf99ca75a82fed6
Brett Naul <brettnaul@gmail.com>
2016-03-24 12:58:36 -0700

    Fix nginx permissions issues on Travis

--- .drone/travis_install.sh
+++ .drone/travis_install.sh
@@ -22,28 +22,6 @@ section_end "install.base.requirements"
 
 
 section "install.cesium.requirements"
-# RethinkDB
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get install rethinkdb -y --force-yes
-
-# RabbitMQ (http://www.scotthelm.com/2013/11/27/rabbit-mq-and-erlang-and-ubuntu-oh-my.html)
-sudo dpkg-divert --local --rename --add /sbin/initctl
-sudo ln -s /bin/true /sbin/initctl
-sudo apt-get purge -y rabbitmq-server
-sudo apt-get autoremove -y rabbitmq-server
-source /etc/lsb-release && echo "deb http://packages.erlang-solutions.com/ubuntu $DISTRIB_CODENAME contrib" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-sudo apt-key add erlang_solutions.asc
-sudo apt-get update
-sudo apt-get install -y erlang erlang-nox
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.2.1/rabbitmq-server_3.2.1-1_all.deb
-sudo dpkg -i rabbitmq-server_3.2.1-1_all.deb
-
-# Newer HDF5/netCDF headers
-sudo apt-get update -qq
-sudo apt-get install libhdf5-7 libhdf5-serial-dev libnetcdf7 libnetcdf-dev -y --force-yes
-
 # Python requirements
 sed -i 's/>=/==/g' requirements.txt
 WHEELHOUSE="--no-index --trusted-host travis-wheels.scikit-image.org \
@@ -68,38 +46,3 @@ section "configure.cesium"
 pip install -e .
 cesium --install
 section_end "configure.cesium"
-
-
-section "configure.services"
-sudo rabbitmq-server &
-make db && sleep 1
-cesium --db-init
-section_end "configure.services"
-
-#echo "[Drone] Build HTML documentation"
-#set +e
-#errors=`make html 2>&1 | tee errors.log | grep -i error`
-#set -e
-#cat errors.log
-#if [[ -n $errors ]]; then
-#    echo "Errors detected in Sphinx build; exiting..."
-#    exit 1;
-#fi
-
-
-section "install.testing.tools"
-# Use pre-packaged 1.9.8 for now
-phantomjs --version
-#( cd /tmp && \
-#wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 && \
-#tar xjf phantomjs-*.tar.bz2 && \
-#sudo rm -f /usr/local/bin/phantomjs && \
-#sudo ln -s /tmp/phantomjs-*/bin/phantomjs /usr/local/bin/phantomjs )
-section_end "install.testing.tools"
-
-
-section "start.nginx"
-sudo mkdir -p /var/lib/nginx/body
-sudo chmod 777 /var/lib/nginx /var/lib/nginx/body
-( cd web_client ; sudo nginx -c nginx.conf -p . -g "daemon off;" & )
-section_end "start.nginx"
